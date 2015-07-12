31186394d93cead013de36747a01dabd5d1898b0
okwuokenye <okwuokenye@gmal.com>
2015-07-12 22:31:28 +0100

    Initial Commit
    Elizade Mobile CRM

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/new/Documents/dev/elizadeCRM/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/new/Documents/dev/elizadeCRM/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+        "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+        "clobbers": [
+            "window.plugins.nativepagetransitions"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.telerik.plugins.nativepagetransitions": "0.4.1"
+}
+// BOTTOM OF METADATA
+});
